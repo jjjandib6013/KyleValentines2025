9d7045ec0e47923c9ffa711d443f4ac75595c003
jandib <142757988+jjjandib6013@users.noreply.github.com>
2025-02-09 01:59:32 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+const messages = [
+    "Octo said click Yes",
+    "Are you sure?",
+    "Really sure??",
+    "Baby please...",
+    "Just think about it!",
+    "If you say no, Octo and me will be really sad...",
+    "Octo will be very sad...",
+    "Me will be very very very sad...",
+    "Ok fine, I will stop asking...",
+    "Just kidding, say yes please! ❤️"
+];
+
+let messageIndex = 0;
+
+function handleNoClick() {
+    const noButton = document.querySelector('.no-button');
+    const yesButton = document.querySelector('.yes-button');
+    noButton.textContent = messages[messageIndex];
+    messageIndex = (messageIndex + 1) % messages.length;
+    const currentSize = parseFloat(window.getComputedStyle(yesButton).fontSize);
+    yesButton.style.fontSize = `${currentSize * 1.5}px`;
+}
+
+function handleYesClick() {
+    window.location.href = "yes_page.html";
+}
