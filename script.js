3402003db58f1c159677c9fed06a3542e5732e43
jandib <142757988+jjjandib6013@users.noreply.github.com>
2025-02-09 16:08:18 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const messages = [
-    "Octo said click Yes",
-    "Are you sure?",
-    "Really sure??",
-    "Baby please...",
-    "Just think about it!",
-    "If you say no, Octo and me will be really sad...",
-    "Octo will be very sad...",
-    "Me will be very very very sad...",
-    "Ok fine, I will stop asking...",
-    "Just kidding, say yes please! ❤️"
+    "Please, just one conversation.",
+    "I just want to understand...",
+    "If this is really the end, let’s not leave it like this.",
+    "I still care about you, no matter what.",
+    "Even if we can't fix this, can we at least talk?",
+    "I don’t want to walk away with regrets...",
+    "Can we just have one last honest conversation?",
+    "Just hear me out, that’s all I ask.",
+    "Okay… if you’re sure, I won’t push anymore.",
+    "But if you ever change your mind, I’ll still be here."
 ];
 
 let messageIndex = 0;
@@ -16,10 +16,16 @@ let messageIndex = 0;
 function handleNoClick() {
     const noButton = document.querySelector('.no-button');
     const yesButton = document.querySelector('.yes-button');
+    const gifImage = document.querySelector('.gif_container img');
     noButton.textContent = messages[messageIndex];
     messageIndex = (messageIndex + 1) % messages.length;
     const currentSize = parseFloat(window.getComputedStyle(yesButton).fontSize);
     yesButton.style.fontSize = `${currentSize * 1.5}px`;
+
+    gifImage.src = "https://media.tenor.com/ggokQeI5AMMAAAAj/sanrio-white.gif";
+
+    gifImage.style.width = "200px";
+    gifImage.style.height = "auto";
 }
 
 function handleYesClick() {
